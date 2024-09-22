415278ea865084a8054cc3321474249f08ebe43d
williamdev <william-ca@live.no>
2024-09-22 10:55:03 +0200

    Better frontend

--- Frontend/src/Layout.tsx
+++ Frontend/src/Layout.tsx
@@ -1,4 +1,52 @@
 import {Link, useNavigate, useParams} from "react-router-dom";
+import IconLayoutDashboard from "./components/icons/IconLayoutDashboard.tsx";
+import IconBxsDashboard from "./components/icons/IconBxsDashboard.tsx";
+import IconListCircleOutline from "./components/icons/IconListCircleOutline.tsx";
+import IconListCircle from "./components/icons/IconListCircle.tsx";
+import IconUser from "./components/icons/IconUser.tsx";
+import IconUserOutline from "./components/icons/IconUserOutline.tsx";
+import IconNotificationsOutline from "./components/icons/IconNotificationsOutline.tsx";
+import IconNotifications from "./components/icons/IconNotifications.tsx";
+import IconSecurePaymentLine from "./components/icons/IconSecurePaymentLine.tsx";
+import IconSecurePaymentFill from "./components/icons/IconSecurePaymentFill.tsx";
+import IconBusiness from "./components/icons/IconBusiness.tsx";
+import IconAccountPlus from "./components/icons/IconAccountPlus.tsx";
+import IconNotification from "./components/icons/IconNotification.tsx";
+
+const tabs = [
+    {
+        title: "Dashboard",
+        url: "/app",
+        icon:  <IconLayoutDashboard width={"1.4em"} height={"1.4em"}/>,
+        iconActive: <IconBxsDashboard width={"1.4em"} height={"1.4em"}/>
+    },
+    {
+        title: "Selskapsoversikt",
+        url: "/app/selskapsoversikt",
+        icon:  <IconListCircleOutline width={"1.4em"} height={"1.4em"}/>,
+        iconActive: <IconListCircle width={"1.4em"} height={"1.4em"}/>
+    },
+    {
+        title: "Profil", url: "/app/profil", icon: <IconUserOutline width={"1.4em"} height={"1.4em"}/>,
+        iconActive: <IconUser width={"1.4em"} height={"1.4em"}/>
+    },
+    /*{
+        title: "Innstillinger", url: "/app/innstillinger", icon: <IconSettingsOutline width={"1.4em"} height={"1.4em"}/>,
+        iconActive: <IconSettings width={"1.4em"} height={"1.4em"}/>
+    },*/
+    {
+        title: "Varsler", url: "/app/varsler", icon: <IconNotificationsOutline width={"1.4em"} height={"1.4em"}/>,
+        iconActive: <IconNotifications width={"1.4em"} height={"1.4em"} />
+    },
+    {
+        title: "Abonnement",
+        url: "/app/abonnement",
+        icon: <IconSecurePaymentLine width={"1.4em"} height={"1.4em"}/>,
+        iconActive: <IconSecurePaymentFill width={"1.4em"} height={"1.4em"}/>
+    },
+]
+
+
 
 export default function Layout({children}) {
     const navigate = useNavigate();
@@ -14,10 +62,10 @@ export default function Layout({children}) {
                         <img src={"/logo.png"}/>
                     </div>
                     <ul className="space-y-2 p-4">
-                        <li onClick={() => {navigate(`/app/workspace/${workspaceId}`)}}><a className="block py-2 px-4 text-gray-700 hover:bg-gray-200 rounded">Dashboard</a></li>
-                        <li onClick={() => {navigate(`/app/workspace/${workspaceId}/selskaper`)}}><a className="block py-2 px-4 text-gray-700 hover:bg-gray-200 rounded">Selskaper</a></li>
-                        <li onClick={() => {navigate(`/app/workspace/${workspaceId}/leads`)}}><a className="block py-2 px-4 text-gray-700 hover:bg-gray-200 rounded">Leads</a></li>
-                        <li onClick={() => {navigate(`/app/workspace/${workspaceId}/aileadassist`)}}><a className="block py-2 px-4 text-gray-700 hover:bg-gray-200 rounded">AI Lead assist</a></li>
+                        <li onClick={() => {navigate(`/app/workspace/${workspaceId}`)}}><a className="cursor-pointer flex py-2 px-4 text-gray-700 hover:bg-gray-200 rounded"><span className={"pt-0.5 pr-2"}><IconLayoutDashboard width={"1.4em"} height={"1.4em"}/></span> Dashboard</a></li>
+                        <li onClick={() => {navigate(`/app/workspace/${workspaceId}/selskaper`)}}><a className="cursor-pointer flex py-2 px-4 text-gray-700 hover:bg-gray-200 rounded"><span className={"pt-0.5 pr-2"}><IconListCircleOutline width={"1.4em"} height={"1.4em"}/></span>Selskaper</a></li>
+                        <li onClick={() => {navigate(`/app/workspace/${workspaceId}/leads`)}}><a className="cursor-pointer flex py-2 px-4 text-gray-700 hover:bg-gray-200 rounded"><span className={"pt-0.5 pr-2"}><IconAccountPlus width={"1.4em"} height={"1.4em"}/></span>Leads</a></li>
+                        <li onClick={() => {navigate(`/app/workspace/${workspaceId}/aileadassist`)}}><a className="cursor-pointer flex py-2 px-4 text-gray-700 hover:bg-gray-200 rounded"><span className={"pt-0.5 pr-2"}><IconNotification width={"1.4em"} height={"1.4em"}/></span>AI Lead assist</a></li>
                     </ul>
                 </nav>
 
