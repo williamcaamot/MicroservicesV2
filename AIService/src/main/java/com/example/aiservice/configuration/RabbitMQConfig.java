a44dc7aa96c6010a983a05d1d79e8f9729072cdb
williamcaamot <william-ca@live.no>
2024-10-03 08:37:23 +0200

    Add AIService

---
+++ AIService/src/main/java/com/example/aiservice/configuration/RabbitMQConfig.java
@@ -0,0 +1,38 @@
+package com.example.aiservice.configuration;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+
+    public static final String EXCHANGE_NAME = "dealflow";
+
+    public static final String QUEUE_NAME = "ai_sales_pitch";
+
+    public static final String ROUTING_KEY = "my_routing_key";
+
+
+    @Bean //1 Declare exchange
+    public Exchange dealflowExchange(){
+        return ExchangeBuilder.directExchange(EXCHANGE_NAME).durable(true).build();
+    }
+
+    @Bean
+    public Queue myQueue(){
+        return QueueBuilder.durable(QUEUE_NAME).build();
+    }
+
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(myQueue())
+                .to(dealflowExchange())
+                .with(ROUTING_KEY)
+                .noargs();
+    }
+
+
+}
---
+++ AIService/src/main/java/com/example/aiservice/service/RabbitMQService.java
@@ -0,0 +1,21 @@
+package com.example.aiservice.service;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQService {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public RabbitMQService(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+
+
+
+
+}
