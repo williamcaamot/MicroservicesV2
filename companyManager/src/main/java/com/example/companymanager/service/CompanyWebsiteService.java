daf5c518e2b3c849691479b59088f9f07fdc93ff
williamcaamot <william-ca@live.no>
2024-10-24 13:07:22 +0200

    Trying to build Auth Service

--- companyManager/src/main/java/com/example/companymanager/service/CompanyWebsiteService.java
+++ companyManager/src/main/java/com/example/companymanager/service/CompanyWebsiteService.java
@@ -3,7 +3,9 @@ package com.example.companymanager.service;
 
 import com.example.companymanager.dto.CompanyWebsiteDTO;
 import com.example.companymanager.entity.Company;
+import com.example.companymanager.entity.Workspace;
 import com.example.companymanager.repository.CompanyRepository;
+import com.example.companymanager.repository.WorkspaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 public class CompanyWebsiteService {
 
     private CompanyRepository companyRepository;
+    private WorkspaceRepository workspaceRepository;
 
     @Autowired
     public CompanyWebsiteService(CompanyRepository companyRepository){
@@ -18,6 +21,11 @@ public class CompanyWebsiteService {
     }
 
     public void SaveCompanyWebsite(CompanyWebsiteDTO websiteDTO){
+
+        // TODO First check that the account has access to the workspace
+        // Then check that the company is part of the workspace!
+
+
         Company company = companyRepository.getById(websiteDTO.getCompanyId());
         company.setHjemmeside(websiteDTO.getCompanyWebsite());
         companyRepository.save(company);
